755999ba38cedcd98d911b3629a0832caee147cc
Risheak Chauhan <risheak@popup.store>
2023-11-28 21:52:12 +0530

    edit sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: "#{REDIS_URL}:7520/0" }
+  config.redis = { url: "#{ENV['REDIS_URL']}:7520/0" }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: "#{REDIS_URL}:7520/0" }
+  config.redis = { url: "#{ENV['REDIS_URL']}:7520/0" }
 end

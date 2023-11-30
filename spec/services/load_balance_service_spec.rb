7935a845ddbecb33f2e2833ac11d44a279a85054
Risheak Chauhan <risheak@popup.store>
2023-11-30 20:28:38 +0530

    Fix rspec

--- spec/services/load_balance_service_spec.rb
+++ spec/services/load_balance_service_spec.rb
@@ -1,3 +1,4 @@
+# spec/services/load_balance_service_spec.rb
 require 'rails_helper'
 
 RSpec.describe LoadBalanceService do
@@ -5,17 +6,18 @@ RSpec.describe LoadBalanceService do
     let(:servers) { ['http://server1.com', 'http://server2.com'] }
 
     before do
-      allow(Server).to receive(:pluck).with(:url).and_return(servers)
-      allow(Rails.cache).to receive(:fetch).with('load_balancer_cache').and_return(0)
+      allow(LoadBalanceService).to receive(:get_servers).and_return(servers)
     end
 
-    it 'distributes requests equally between two servers' do
-      100.times do |i|
-        mod = (i % 2)
-        server_number = mod + 1
-        allow(Rails.cache).to receive(:read).with('load_balancer_cache').and_return(mod)
-        expect(servers[mod]).to eq("http://server#{server_number}.com")
+    it 'balances requests between servers' do
+      server_counts = { 'http://server1.com' => 0, 'http://server2.com' => 0 }
+
+      100.times do
+        url = LoadBalanceService.next_server_url
+        server_counts[url] += 1
       end
+      expect(server_counts['http://server1.com']).to eq(50)
+      expect(server_counts['http://server2.com']).to eq(50)
     end
   end
 end

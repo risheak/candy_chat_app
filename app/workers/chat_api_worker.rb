ee0c8a6e271abee96ed2d58123309bd5aba53cef
Risheak Chauhan <risheak@popup.store>
2023-12-01 07:50:01 +0530

    remove more unwanted code

--- app/workers/chat_api_worker.rb
+++ app/workers/chat_api_worker.rb
@@ -3,19 +3,5 @@ class ChatApiWorker
 
   def perform(profile_id, message)
     response = ChatApiService.send_message(message, profile_id)
-    # broadcast_turbo_stream(message)
-  end
-
-  private
-
-  def broadcast_turbo_stream(message)
-    broadcast_args = {
-      action: :replace,
-      target: "message_#{message.id}",
-      partial: "messages/message",
-      locals: { message: message }
-    }
-
-    Turbo::StreamsChannel.broadcast_to("messages_list", broadcast_args)
   end
 end
